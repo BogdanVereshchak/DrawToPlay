7abd6d737cba3beeb3eb58d9c503ba686d29d9f6
Kharachko <volodymyr.kharachko.pp.2022@lpnu.ua>
2025-12-22 04:17:28 +0200

    ya topik

--- LevelBuilder.gd
+++ LevelBuilder.gd
@@ -1,10 +1,8 @@
+@tool
 extends Node2D
 
-# Шлях до файлу, який згенерував Python
-const LEVEL_DATA_PATH = "res://level_data.json" 
-# Якщо Python зберігає ззовні проєкту, використовуй user:// або абсолютний шлях
+const LEVEL_DATA_PATH = "res://level_data.json"
 
-# Бібліотека префабів (перетягни сюди свої сцени в Інспекторі)
 @export var platform_scene: PackedScene
 @export var spike_scene: PackedScene
 @export var enemy_scene: PackedScene
@@ -13,32 +11,44 @@ const LEVEL_DATA_PATH = "res://level_data.json"
 @export var finish_scene: PackedScene
 @export var box_scene: PackedScene
 
+# Посилання на контейнери
+@onready var containers = {
+	"platform": $Platforms,
+	"spikes": $Spikes,
+	"enemy": $Enemies,
+	"coin": $Coins,
+	"player_start": $PlayerStart,
+	"finish": $Finish,
+	"box": $Boxes
+}
+
 func _ready():
 	load_level()
 
 func load_level():
-	# 1. Читаємо файл
 	if not FileAccess.file_exists(LEVEL_DATA_PATH):
-		print("Файл рівня не знайдено! Запустіть спочатку Python-сканер.")
+		print("Помилка: Файл рівня не знайдено.")
 		return
 
 	var file = FileAccess.open(LEVEL_DATA_PATH, FileAccess.READ)
-	var content = file.get_as_text()
-	
-	# 2. Парсимо JSON
 	var json = JSON.new()
-	var error = json.parse(content)
+	var error = json.parse(file.get_as_text())
 	
 	if error != OK:
 		print("Помилка парсингу JSON")
 		return
 		
 	var data = json.data
-	var objects = data["objects"]
 	
-	# 3. Будуємо світ
-	for obj in objects:
+	_clear_level()
+	
+	for obj in data["objects"]:
 		spawn_object(obj)
+
+func _clear_level():
+	for container in containers.values():
+		for child in container.get_children():
+			child.queue_free()
 
 func spawn_object(obj_data):
 	var type = obj_data["type"]
@@ -46,41 +56,30 @@ func spawn_object(obj_data):
 	var y = obj_data["y"]
 	var w = obj_data["width"]
 	var h = obj_data["height"]
+	var rot = obj_data.get("rotation", 0.0)
 	
 	var instance = null
 	
 	match type:
 		"platform":
 			instance = platform_scene.instantiate()
-			# Масштабуємо платформу під розмір малюнка
-			# Припускаємо, що спрайт платформи має розмір 100x100 пікселів за замовчуванням
-			# Тобі треба буде налаштувати скейлінг під свої спрайти
-			# instance.scale = Vector2(w / 100.0, h / 100.0) 
-			
-			# АБО краще використовувати NinePatchRect або змінювати shape колайдера
 			_resize_platform_collider(instance, w, h)
-			
-		"enemy":
-			instance = enemy_scene.instantiate()
-		"coin":
-			instance = coin_scene.instantiate()
-		"player_start":
-			instance = player_scene.instantiate()
-		"finish":
-			instance = finish_scene.instantiate()
-		"spikes":
-			instance = spike_scene.instantiate()
-		"box":
-			instance = box_scene.instantiate()
-			
+		"enemy": instance = enemy_scene.instantiate()
+		"coin": instance = coin_scene.instantiate()
+		"player_start": instance = player_scene.instantiate()
+		"finish": instance = finish_scene.instantiate()
+		"spikes": instance = spike_scene.instantiate()
+		"box": instance = box_scene.instantiate()
+		
 	if instance:
 		instance.position = Vector2(x, y)
-		add_child(instance)
+		instance.rotation_degrees = rot
+		
+		if containers.has(type):
+			containers[type].add_child(instance)
+		else:
+			add_child(instance)
 
-# Допоміжна функція для зміни розміру колайдера платформи
 func _resize_platform_collider(platform_instance, w, h):
-	# Це приклад. У твоїй сцені платформи має бути Sprite2D і CollisionShape2D
-	# Змінюємо масштаб ноди, це найпростіший спосіб для прототипу
-	# Але краще мати базовий спрайт розміром 1х1 або 32х32 і множити
-	var base_size = 64.0 # Припустимо, твій тайл 32px
+	var base_size = 64.0 
 	platform_instance.scale = Vector2(w / base_size, h / base_size)
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,11 +1,47 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var speed = 300.0
+@export var jump_velocity = -400.0
+@export var dash_speed = 1000.0
+@export var dash_duration = 0.2
+@export var dash_cooldown = 0.8
 
-func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var is_dashing = false
+var can_dash = true
 
-func _physics_process(_delta):
-	get_input()
+func _physics_process(delta):
+	if is_dashing:
+		move_and_slide()
+		return
+
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+
+	var direction = Input.get_axis("left", "right")
+	
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+
+	if Input.is_action_just_pressed("dash") and can_dash and direction != 0:
+		start_dash(direction)
+
 	move_and_slide()
+
+func start_dash(direction):
+	is_dashing = true
+	can_dash = false
+	
+	velocity = Vector2(direction * dash_speed, 0)
+	
+	await get_tree().create_timer(dash_duration).timeout
+	is_dashing = false
+	velocity.x = move_toward(velocity.x, 0, speed)
+	
+	await get_tree().create_timer(dash_cooldown).timeout
+	can_dash = true
